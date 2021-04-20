c2d1c23b22655e8447df4b9a47ea275f8363ac28
jeocarlolubao <jeocarlolubao@gmail.com>
2021-04-20 17:35:20 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace PF101PROJECT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tictactoe_00 = new System.Windows.Forms.PictureBox();
+            this.tictactoe_01 = new System.Windows.Forms.PictureBox();
+            this.tictactoe_02 = new System.Windows.Forms.PictureBox();
+            this.tictactoe_10 = new System.Windows.Forms.PictureBox();
+            this.tictactoe_11 = new System.Windows.Forms.PictureBox();
+            this.tictactoe_12 = new System.Windows.Forms.PictureBox();
+            this.tictactoe_20 = new System.Windows.Forms.PictureBox();
+            this.tictactoe_21 = new System.Windows.Forms.PictureBox();
+            this.tictactoe_22 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_22)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(230, 161);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // tictactoe_00
+            // 
+            this.tictactoe_00.BackColor = System.Drawing.Color.Transparent;
+            this.tictactoe_00.InitialImage = ((System.Drawing.Image)(resources.GetObject("tictactoe_00.InitialImage")));
+            this.tictactoe_00.Location = new System.Drawing.Point(246, 177);
+            this.tictactoe_00.Name = "tictactoe_00";
+            this.tictactoe_00.Size = new System.Drawing.Size(140, 140);
+            this.tictactoe_00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tictactoe_00.TabIndex = 5;
+            this.tictactoe_00.TabStop = false;
+            this.tictactoe_00.Click += new System.EventHandler(this.tictactoe_x00_Click);
+            // 
+            // tictactoe_01
+            // 
+            this.tictactoe_01.BackColor = System.Drawing.Color.Transparent;
+            this.tictactoe_01.InitialImage = ((System.Drawing.Image)(resources.GetObject("tictactoe_01.InitialImage")));
+            this.tictactoe_01.Location = new System.Drawing.Point(411, 177);
+            this.tictactoe_01.Name = "tictactoe_01";
+            this.tictactoe_01.Size = new System.Drawing.Size(140, 140);
+            this.tictactoe_01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tictactoe_01.TabIndex = 14;
+            this.tictactoe_01.TabStop = false;
+            this.tictactoe_01.Click += new System.EventHandler(this.tictactoe_01_Click);
+            // 
+            // tictactoe_02
+            // 
+            this.tictactoe_02.BackColor = System.Drawing.Color.Transparent;
+            this.tictactoe_02.InitialImage = ((System.Drawing.Image)(resources.GetObject("tictactoe_02.InitialImage")));
+            this.tictactoe_02.Location = new System.Drawing.Point(575, 177);
+            this.tictactoe_02.Name = "tictactoe_02";
+            this.tictactoe_02.Size = new System.Drawing.Size(140, 140);
+            this.tictactoe_02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tictactoe_02.TabIndex = 15;
+            this.tictactoe_02.TabStop = false;
+            this.tictactoe_02.Click += new System.EventHandler(this.tictactoe_02_Click);
+            // 
+            // tictactoe_10
+            // 
+            this.tictactoe_10.BackColor = System.Drawing.Color.Transparent;
+            this.tictactoe_10.InitialImage = ((System.Drawing.Image)(resources.GetObject("tictactoe_10.InitialImage")));
+            this.tictactoe_10.Location = new System.Drawing.Point(246, 342);
+            this.tictactoe_10.Name = "tictactoe_10";
+            this.tictactoe_10.Size = new System.Drawing.Size(140, 140);
+            this.tictactoe_10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tictactoe_10.TabIndex = 16;
+            this.tictactoe_10.TabStop = false;
+            this.tictactoe_10.Click += new System.EventHandler(this.tictactoe_10_Click);
+            // 
+            // tictactoe_11
+            // 
+            this.tictactoe_11.BackColor = System.Drawing.Color.Transparent;
+            this.tictactoe_11.InitialImage = ((System.Drawing.Image)(resources.GetObject("tictactoe_11.InitialImage")));
+            this.tictactoe_11.Location = new System.Drawing.Point(411, 342);
+            this.tictactoe_11.Name = "tictactoe_11";
+            this.tictactoe_11.Size = new System.Drawing.Size(140, 140);
+            this.tictactoe_11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tictactoe_11.TabIndex = 17;
+            this.tictactoe_11.TabStop = false;
+            this.tictactoe_11.Click += new System.EventHandler(this.tictactoe_11_Click);
+            // 
+            // tictactoe_12
+            // 
+            this.tictactoe_12.BackColor = System.Drawing.Color.Transparent;
+            this.tictactoe_12.InitialImage = ((System.Drawing.Image)(resources.GetObject("tictactoe_12.InitialImage")));
+            this.tictactoe_12.Location = new System.Drawing.Point(575, 342);
+            this.tictactoe_12.Name = "tictactoe_12";
+            this.tictactoe_12.Size = new System.Drawing.Size(140, 140);
+            this.tictactoe_12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tictactoe_12.TabIndex = 18;
+            this.tictactoe_12.TabStop = false;
+            this.tictactoe_12.Click += new System.EventHandler(this.tictactoe_12_Click);
+            // 
+            // tictactoe_20
+            // 
+            this.tictactoe_20.BackColor = System.Drawing.Color.Transparent;
+            this.tictactoe_20.InitialImage = ((System.Drawing.Image)(resources.GetObject("tictactoe_20.InitialImage")));
+            this.tictactoe_20.Location = new System.Drawing.Point(246, 509);
+            this.tictactoe_20.Name = "tictactoe_20";
+            this.tictactoe_20.Size = new System.Drawing.Size(140, 140);
+            this.tictactoe_20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tictactoe_20.TabIndex = 19;
+            this.tictactoe_20.TabStop = false;
+            this.tictactoe_20.Click += new System.EventHandler(this.tictactoe_20_Click);
+            // 
+            // tictactoe_21
+            // 
+            this.tictactoe_21.BackColor = System.Drawing.Color.Transparent;
+            this.tictactoe_21.InitialImage = ((System.Drawing.Image)(resources.GetObject("tictactoe_21.InitialImage")));
+            this.tictactoe_21.Location = new System.Drawing.Point(411, 509);
+            this.tictactoe_21.Name = "tictactoe_21";
+            this.tictactoe_21.Size = new System.Drawing.Size(140, 140);
+            this.tictactoe_21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tictactoe_21.TabIndex = 20;
+            this.tictactoe_21.TabStop = false;
+            this.tictactoe_21.Click += new System.EventHandler(this.tictactoe_21_Click);
+            // 
+            // tictactoe_22
+            // 
+            this.tictactoe_22.BackColor = System.Drawing.Color.Transparent;
+            this.tictactoe_22.InitialImage = ((System.Drawing.Image)(resources.GetObject("tictactoe_22.InitialImage")));
+            this.tictactoe_22.Location = new System.Drawing.Point(575, 509);
+            this.tictactoe_22.Name = "tictactoe_22";
+            this.tictactoe_22.Size = new System.Drawing.Size(140, 140);
+            this.tictactoe_22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tictactoe_22.TabIndex = 21;
+            this.tictactoe_22.TabStop = false;
+            this.tictactoe_22.Click += new System.EventHandler(this.tictactoe_22_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(497, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(433, 56);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Winner Label Text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.ClientSize = new System.Drawing.Size(942, 673);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tictactoe_22);
+            this.Controls.Add(this.tictactoe_21);
+            this.Controls.Add(this.tictactoe_20);
+            this.Controls.Add(this.tictactoe_12);
+            this.Controls.Add(this.tictactoe_11);
+            this.Controls.Add(this.tictactoe_10);
+            this.Controls.Add(this.tictactoe_02);
+            this.Controls.Add(this.tictactoe_01);
+            this.Controls.Add(this.tictactoe_00);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "TIC - TAC - TOE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tictactoe_22)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox tictactoe_00;
+        private System.Windows.Forms.PictureBox tictactoe_01;
+        private System.Windows.Forms.PictureBox tictactoe_02;
+        private System.Windows.Forms.PictureBox tictactoe_10;
+        private System.Windows.Forms.PictureBox tictactoe_11;
+        private System.Windows.Forms.PictureBox tictactoe_12;
+        private System.Windows.Forms.PictureBox tictactoe_20;
+        private System.Windows.Forms.PictureBox tictactoe_21;
+        private System.Windows.Forms.PictureBox tictactoe_22;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PF101PROJECT
+{
+    public partial class Form1 : Form
+    {
+
+
+        int[] GameArrayGrid = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        bool handlePlayerTurn = true; //true = o false = x
+
+        Bitmap circle = Properties.Resources.o_symbol;
+        Bitmap cross = Properties.Resources.x_symbol;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeGameState();
+
+        }
+
+        private void InitializeGameState()
+        {
+            int[] GameArrayGrid = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tictactoe_x00_Click(object sender, EventArgs e)
+        {
+            if (handlePlayerTurn)
+            {
+                if (GameArrayGrid[0] == 0)
+                {
+                    GameArrayGrid[0] = 1;
+                    tictactoe_00.Image = circle;
+                    handlePlayerTurn = false;
+                }
+            }
+            else
+            {
+                if (GameArrayGrid[0] == 0)
+                {
+                    GameArrayGrid[0] = -1;
+                    tictactoe_00.Image = cross;
+                    handlePlayerTurn = true;
+                }
+            }
+        }
+
+        private void tictactoe_01_Click(object sender, EventArgs e)
+        {
+            if (handlePlayerTurn)
+            {
+                if (GameArrayGrid[1] == 0)
+                {
+                    GameArrayGrid[1] = 1;
+                    tictactoe_01.Image = circle;
+                    handlePlayerTurn = false;
+                }
+            }
+            else
+            {
+                if (GameArrayGrid[1] == 0)
+                {
+                    GameArrayGrid[1] = -1;
+                    tictactoe_01.Image = cross;
+                    handlePlayerTurn = true;
+                }
+            }
+        }
+
+        private void tictactoe_02_Click(object sender, EventArgs e)
+        {
+            if (handlePlayerTurn)
+            {
+                if (GameArrayGrid[2] == 0)
+                {
+                    GameArrayGrid[2] = 1;
+                    tictactoe_02.Image = circle;
+                    handlePlayerTurn = false;
+                }
+            }
+            else
+            {
+                if (GameArrayGrid[2] == 0)
+                {
+                    GameArrayGrid[2] = -1;
+                    tictactoe_02.Image = cross;
+                    handlePlayerTurn = true;
+                }
+            }
+            
+        }
+
+        private void tictactoe_10_Click(object sender, EventArgs e)
+        {
+            if (handlePlayerTurn)
+            {
+                if (GameArrayGrid[3] == 0)
+                {
+                    GameArrayGrid[3] = 1;
+                    tictactoe_10.Image = circle;
+                    handlePlayerTurn = false;
+                }
+            }
+            else
+            {
+                if (GameArrayGrid[3] == 0)
+                {
+                    GameArrayGrid[3] = -1;
+                    tictactoe_10.Image = cross;
+                    handlePlayerTurn = true;
+                }
+            }
+        }
+
+        private void tictactoe_11_Click(object sender, EventArgs e)
+        {
+            if (handlePlayerTurn)
+            {
+                if (GameArrayGrid[4] == 0)
+                {
+                    GameArrayGrid[4] = 1;
+                    tictactoe_11.Image = circle;
+                    handlePlayerTurn = false;
+                }
+            }
+            else
+            {
+                if (GameArrayGrid[4] == 0)
+                {
+                    GameArrayGrid[4] = -1;
+                    tictactoe_11.Image = cross;
+                    handlePlayerTurn = true;
+                }
+            }
+        }
+
+        private void tictactoe_12_Click(object sender, EventArgs e)
+        {
+            if (handlePlayerTurn)
+            {
+                if (GameArrayGrid[5] == 0)
+                {
+                    GameArrayGrid[5] = 1;
+                    tictactoe_12.Image = circle;
+                    handlePlayerTurn = false;
+                }
+            }
+            else
+            {
+                if (GameArrayGrid[5] == 0)
+                {
+                    GameArrayGrid[5] = -1;
+                    tictactoe_12.Image = cross;
+                    handlePlayerTurn = true;
+                }
+            }
+        }
+
+        private void tictactoe_20_Click(object sender, EventArgs e)
+        {
+            if (handlePlayerTurn)
+            {
+                if (GameArrayGrid[6] == 0)
+                {
+                    GameArrayGrid[6] = 1;
+                    tictactoe_20.Image = circle;
+                    handlePlayerTurn = false;
+                }
+            }
+            else
+            {
+                if (GameArrayGrid[6] == 0)
+                {
+                    GameArrayGrid[6] = -1;
+                    tictactoe_20.Image = cross;
+                    handlePlayerTurn = true;
+                }
+            }
+        }
+
+        private void tictactoe_21_Click(object sender, EventArgs e)
+        {
+            if (handlePlayerTurn)
+            {
+                if (GameArrayGrid[7] == 0)
+                {
+                    GameArrayGrid[7] = 1;
+                    tictactoe_21.Image = circle;
+                    handlePlayerTurn = false;
+                }
+            }
+            else
+            {
+                if (GameArrayGrid[7] == 0)
+                {
+                    GameArrayGrid[7] = -1;
+                    tictactoe_21.Image = cross;
+                    handlePlayerTurn = true;
+                }
+            }
+        }
+
+        private void tictactoe_22_Click(object sender, EventArgs e)
+        {
+            if (handlePlayerTurn)
+            {
+                if (GameArrayGrid[8] == 0)
+                {
+                    GameArrayGrid[8] = 1;
+                    tictactoe_22.Image = circle;
+                    handlePlayerTurn = false;
+                }
+            }
+            else
+            {
+                if (GameArrayGrid[8] == 0)
+                {
+                    GameArrayGrid[8] = -1;
+                    tictactoe_22.Image = cross;
+                    handlePlayerTurn = true;
+                }
+            }
+        }
+    }
+}
